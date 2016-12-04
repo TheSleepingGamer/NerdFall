b77ecc5f9e599ede51d74e0e3d39ec478e9aa5e6
Aleksandar Tanev <aleksander.tanev.51@gmail.com>
2016-12-04 14:36:20 +0200

    Fall scene - added new cannon textures, cannon code logic. Cannon follows the mouse, on left click you reload and fire. This will be improved later.

---
+++ NerdFall/Assets/Scripts/Fall/WeaponController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private GameObject loadedBullet;
+
+    void Update()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - this.transform.position;
+        float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.LoadNewBullet();
+        }
+    }
+
+    public void LoadNewBullet()
+    {
+        if (this.loadedBullet == null)
+        {
+            this.loadedBullet = (GameObject)Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+            this.loadedBullet.transform.parent = this.transform;
+            this.loadedBullet.transform.localPosition = new Vector3(this.loadedBullet.transform.localPosition.x + 1f, this.loadedBullet.transform.localPosition.y);
+        }
+        else
+        {
+            this.FireBullet();
+        }
+    }
+
+    public void FireBullet()
+    {
+        Rigidbody2D rbBullet = this.loadedBullet.GetComponent<Rigidbody2D>();
+        rbBullet.isKinematic = false;
+        rbBullet.AddRelativeForce(new Vector2(10f, 0), ForceMode2D.Impulse);
+        this.loadedBullet.transform.parent = null;
+        Destroy(this.loadedBullet, 3);
+
+        this.loadedBullet = null;
+    }
+
+}
