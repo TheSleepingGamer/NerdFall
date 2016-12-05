584963441cb8a8aea0379f5c6cfa1f33e6d1149a
AleksandarTanev <aleksander.tanev.51@gmail.com>
2016-12-05 17:38:47 +0200

    - Added example script for adding button functionality to a sprite. - Rearranged file structure so that GitIgnore would work.

---
+++ Assets/Scripts/OnClickTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class OnClickTrigger : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click detected!");
+    }
+}
