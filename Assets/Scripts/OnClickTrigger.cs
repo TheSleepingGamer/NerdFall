ae8445b5bf8d7aa836a2357ca91b75dd59b6df39
Teodora.Nozharova <tedinoji@gmail.com>
2016-12-06 22:33:39 +0200

    added simple navigation through scenes "University", "Task info", "Fall"

--- Assets/Scripts/OnClickTrigger.cs
+++ Assets/Scripts/OnClickTrigger.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class OnClickTrigger : MonoBehaviour, IPointerClickHandler
@@ -8,5 +10,23 @@ public class OnClickTrigger : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Click detected!");
+    }
+
+    public void OnScrollClick()
+    {
+        Debug.Log("Click detected!");
+        SceneManager.LoadScene("TaskInfo");
+    }
+
+    public void OnBackButtonClick()
+    {
+        Debug.Log("Click detected!");
+        SceneManager.LoadScene("University");
+    }
+
+    public void OnStartButtonClick()
+    {
+        Debug.Log("Click detected!");
+        SceneManager.LoadScene("Fall");
     }
 }
